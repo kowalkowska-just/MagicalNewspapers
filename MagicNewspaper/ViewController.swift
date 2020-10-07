54d42898ae523c4f1488b826b7aa247788cc59e5
Justyna <kowalkowska.just@gmail.com>
2020-10-08 00:18:45 +0200

    Video playback in AR.

--- MagicNewspaper/ViewController.swift
+++ MagicNewspaper/ViewController.swift
@@ -56,9 +56,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         if let imageAnchor = anchor as? ARImageAnchor {
             
+            let videoNode = SKVideoNode(fileNamed: "HarryPotter.mp4")
+
+            videoNode.play()
+            
+            let videoScene = SKScene(size: CGSize(width: 480, height: 360))
+            
+            videoNode.position = CGPoint(x: videoScene.size.width / 2, y: videoScene.size.height / 2)
+            videoNode.yScale = -1.0
+            
+            videoScene.addChild(videoNode)
+            
             let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
             
-            plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+            plane.firstMaterial?.diffuse.contents = videoScene
             
             let planeNode = SCNNode(geometry: plane)
             
